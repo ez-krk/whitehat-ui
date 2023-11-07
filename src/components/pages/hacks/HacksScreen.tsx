7fb0743e2131b8f6ba0e71cfc16e9d0ad2f65482
ez-krk <dev@krk.finance>
2023-11-07 07:24:12 +0100

    epurated @/components/pages/hacks/HacksScreen.tsx

--- src/components/pages/hacks/HacksScreen.tsx
+++ src/components/pages/hacks/HacksScreen.tsx
@@ -1,26 +1,10 @@
-import { useCallback, useEffect, useState } from 'react'
-import { userState } from '@/store/user'
-import { useRecoilValue } from 'recoil'
-import useToastMessage from '@/hooks/useToastMessage'
-import { useTranslation } from 'next-i18next'
-import { PROTOCOL_PDA } from '@/types'
 import HacksBox from './HacksBox'
 
-export default function DashboardScreen() {
-  const { t } = useTranslation()
-  const [isNewChatModalOpen, setNewChatModalOpen] = useState(false)
-  const [currentChatRoomId, setCurrentChatRoomId] = useState<string | null>(
-    null
-  )
-
-  const user = useRecoilValue(userState)
-
-  const [programs, setPrograms] = useState<PROTOCOL_PDA[]>([])
-
+export default function HacksScreen() {
   return (
     <>
       <div className="content-height flex w-full flex-col items-start justify-start overflow-auto sm:flex-row">
-        <HacksBox currentChatRoomId={currentChatRoomId} />
+        <HacksBox />
       </div>
     </>
   )
